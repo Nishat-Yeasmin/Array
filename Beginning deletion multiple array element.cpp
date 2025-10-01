4b1b84920b6dedf0336a9b1ae682ff798654eccd
Nishat Yeasmin <nishatyasminnisha@gmail.com>
2025-10-01 07:09:44 +0600

    Add files via upload

---
+++ Beginning deletion multiple array element.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i;
+    cout<<"Enter array size: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter array elements: ";
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int l;
+    cout<<"Enter no. of value you want to delete: ";
+    cin>>l;
+    for(i=0;i<n-l;i++)
+    {
+        arr[i] = arr[i+l];
+    }
+    cout<<"Final array: ";
+    for(i=0;i<n-l;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
